cfc08d0e44a84ee7357f14b4c86521fbaada8cd3
Victor Avila <avilavictor1570@gmail.com>
2020-08-05 16:52:51 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,9 +1,16 @@
 class Customer
   attr_reader :name, :age 
   
+  @@all = []
+  
   def initialize
     @name = name
     @age = age
+    @@all << self
+  end
+  
+  def self.all
+    @@all
   end
   
   def new_meal(waiter, total, tip=0)
