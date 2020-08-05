99005a58a0730defd18dccffc3ee2f7fb9168914
Victor Avila <avilavictor1570@gmail.com>
2020-08-05 16:54:15 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -3,7 +3,15 @@ class Waiter
   
   @@all = []
   
-  def initialize
+  def initialize (name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
     
 
 end
